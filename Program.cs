51618f5bb7557bc8ab4ea0a6be7725579f78ab89
Jacob Archambault <jacob.archambault@waystar.com>
2020-01-13 15:23:43 -0500

    Fix after changing folder.

--- Program.cs
+++ Program.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using CarLibrary;
+﻿using CarLibrary;
+using System;
 
 namespace SharedCarLibClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("***** Shared Assembly Client *****");
             SportsCar c = new SportsCar();
